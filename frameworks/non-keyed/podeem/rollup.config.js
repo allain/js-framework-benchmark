4afcc02d62bbf064c8c8fdbd69849540333f1b02
Allain Lalonde <allain.lalonde@gmail.com>
2018-12-28 16:48:54 -0500

    Adding podeem benchmark

---
+++ frameworks/non-keyed/podeem/rollup.config.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import * as path from 'path'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import minifyliterals from 'rollup-plugin-minifyliterals'
+
+export default {
+  input: 'app.js',
+  output: {
+    file: 'dist/app.min.js',
+    format: 'iife',
+    name: 'app',
+    sourcemap: true
+  },
+  plugins: [
+    resolve({
+      module: true,
+      jsnext: true,
+      browser: true
+    }),
+    commonjs({
+      namedExports: {
+        podeem: ['default']
+      }
+    }),
+    minifyliterals(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [['es2016']],
+      plugins: ['external-helpers'],
+      runtimeHelpers: true,
+      babelrc: false
+    }),
+    terser()
+  ]
+}
